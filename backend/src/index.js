0c2874481cdece9aeb5108d0edc03dc387d12de4
alysonmiguel <alysonmiguel16@gmail.com>
2020-01-15 01:30:06 -0300

    Segundo dia da semana omnistack

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express'); 
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/test', {
+  useMongoClient: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 1000
+});
+
+app.use(express.json());
+app.use(routes);
+
+// get, post, put, delete 
+
+// MongoDB (Não-relacional)
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    return response.json({ message: 'Hello OmniStack' });
+});
+
+app.listen(3333);
