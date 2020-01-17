bed1c7f9e4441f19cbf07113b50e6aff1deef7e7
alysonmiguel <alysonmiguel16@gmail.com>
2020-01-16 22:22:29 -0300

    Terceiro dia da semanaomnistack

---
+++ web/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect} from 'react';
+import api from './services/api';
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
+
+import DevItem from './componenets/DevItem'
+import DevForm from './componenets/DevForm'
+
+// Componente: Função que retorna hmtl, css, js e não interfere no restante da aplicação
+// Propiedade: Atributo 
+// Estado: Informações mantidas pelo componente (Estudar : Imutabilidade)
+
+function App() {
+  const [ devs, setDevs] = useState([]);
+
+  
+
+ 
+  useEffect(() => {
+    async function loadDevs(){
+      const response = await api.get('./devs');
+
+      setDevs(response.data);
+    } 
+    loadDevs()
+  }, []);
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data)
+  
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+        <aside>
+          <strong>Cadastrar</strong>
+          <DevForm onSubmit={handleAddDev} />
+        </aside>
+
+        <main>
+          <ul>
+            {devs.map(dev => (
+              <DevItem key={dev._id} dev={dev} />
+            ))}
+          </ul>
+        </main>
+    </div>
+  );
+}
+
+export default App;
